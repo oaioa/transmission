6d8e016cf55f541d7d6b7d4afe543b99f32218b3
oaioa <thomas.viola@insa-lyon.fr>
2017-04-14 15:45:17 +0200

    Version mergée avec début slow start

--- Jady/client.c
+++ Jady/client.c
@@ -1,121 +1,132 @@
 /*
-   Simple udp client
-http://www.binarytides.com/programming-udp-sockets-c-linux/
-*/
+ Simple udp client
+ http://www.binarytides.com/programming-udp-sockets-c-linux/
+ */
 #include "useful.h"
 #include <time.h>
 #include <sys/time.h>
 #define SORTIE "recv" //where i'm stocking what i get
 
-int main(void)
-{
+int main(void) {
 //	
-    struct sockaddr_in si_other;
-    int s;
-    int i;
-    int slen=sizeof(si_other);
-    int compare = 1 ; // il ya une difference
-    int first = 0;
-    
-    char *buf = (char*) malloc(sizeof(char) * FRAGLEN); //temporary buffer to receive ONE fragment
-    char *message = (char*) malloc(sizeof(char) * BUFLEN); 
-    char *buf_file=(char*)malloc(sizeof(char) * 1000000000); 
-    
-    FILE * f_out;
-    int file_i=0;
-    int id_frag = 0;
-	int sent, recv=0;
-	
+	struct sockaddr_in si_other;
+	int s;
+	int i;
+	int slen = sizeof(si_other);
+	int compare = 1; // il ya une difference
+	int first = 0;
+
+	char *buf = (char*) malloc(sizeof(char) * FRAGLEN); //temporary buffer to receive ONE fragment
+	char *message = (char*) malloc(sizeof(char) * BUFLEN);
+	char *buf_file = (char*) malloc(sizeof(char) * 1000000000);
+
+	FILE * f_out;
+	int file_i = 0;
+	int id_frag = 0;
+	int sent, recv = 0;
+
 	//char *SORTIE = (char*) malloc(sizeof(char) * FRAGLEN);
-	
+
 	struct timeval end, start;
 	float delta;
 
-    if (connect_to_port (&s, PORT, &si_other) !=0){
+	int rwnd = 1; //receiver window
+	int cwnd = 10; //congestion window
+	int flightSize = 0; //data that has been sent, but not yet acknowledged
+
+	int j = 0;
+
+	if (connect_to_port(&s, PORT, &si_other) != 0) {
 		die("Premiere connexion échouée\n");
 	}
 
-    while(1)
-    {
-        if(compare ==1){ //processus d'acceptation de connexion	
-            compare = string_compare(buf,strlen(buf)); //comparer le debut avec SYN-ACK
-            if(compare==0){
-                sent = send_message(s,"ACK",3,  (struct sockaddr *)&si_other, slen);
-                int PORT2 = atoi(index(buf,'K')+1);
-                
-                if (connect_to_port (&s, PORT2, &si_other) <0){
+	while (1) {
+		if (compare == 1) { //processus d'acceptation de connexion
+			compare = string_compare(buf, strlen(buf)); //comparer le debut avec SYN-ACK
+			if (compare == 0) {
+				sent = send_message(s, "ACK", 3, (struct sockaddr *) &si_other,
+						slen);
+				int PORT2 = atoi(index(buf, 'K') + 1);
+
+				if (connect_to_port(&s, PORT2, &si_other) < 0) {
 					die("Connexion au serveur-fils échouée\n");
 				}
-            }
-            else{//envoie du SYN
-                sent = send_message(s,"SYN",3,  (struct sockaddr *)&si_other, slen);
-                compare =1;
-            }
-            
-            
-			if (first == 0){ //attente de SYN-ACK_NUMERO_DE_PORT
-				recv = receive_message(s, buf,NULL,0);
+			} else { //envoie du SYN
+				sent = send_message(s, "SYN", 3, (struct sockaddr *) &si_other,
+						slen);
+				compare = 1;
+			}
+
+			if (first == 0) { //attente de SYN-ACK_NUMERO_DE_PORT
+				recv = receive_message(s, buf, NULL, 0);
 				first = -1;
 			}
-        }
-        
-        else{//une fois la connexion acceptée
-            insert_a_message(&message);
-            sent = send_message(s,message,strlen(message),  (struct sockaddr *)&si_other, slen);
-		
+		}
+
+		else { //une fois la connexion acceptée
+			insert_a_message(&message);
+			sent = send_message(s, message, strlen(message),
+					(struct sockaddr *) &si_other, slen);
+
 			printf("\nMode réception de fichier...\n");
 			gettimeofday(&start, NULL);
-			
-			while(1){
-				delete(buf,sizeof(buf));
-				recv = receive_message(s, buf,NULL,0);
-				id_frag = my_atoi(buf,6);//useful for ACK
-				//printf("\nrecu: %d\n",recv);
-				
-				if(strcmp(buf,"FIN")==0){
-					break;
+
+			while (1) {
+				if (flightSize < cwnd) {
+                    printf("Going to receive\n");
+					delete(buf, sizeof(buf));
+					recv = receive_message(s, buf, NULL, 0);
+					id_frag = my_atoi(buf, 6); //useful for ACK
+					printf("\nrecu: %d\n",recv);
+
+					if (strcmp(buf, "FIN") == 0) {
+						break;
+					}
+
+					//filling the buffer
+					memcpy(buf_file + file_i, buf, recv);
+					file_i += recv;
+					flightSize++;
+					printf("flightSize %d for id %d \n", flightSize, id_frag);
+				} else {
+                    printf("Going to send \n");
+					//sending ACK
+					delete(message, sent);
+					sprintf(message, "ACK%0.6d%s", id_frag);
+					sent = send_message(s, message, 20,
+							(struct sockaddr *) &si_other, slen);
+					printf("Envoyé: %s de taille %d\n",message,sent);
+					flightSize = 0;
+
 				}
-				
-				//filling the buffer
-				memcpy(buf_file+file_i,buf,recv);
-				file_i += recv;
-				
-				
-				//sending ACK
-				delete(message,sent);
-				sprintf(message, "ACK%0.6d%s",id_frag);
-				sent = send_message(s,message,20, (struct sockaddr *)&si_other, slen);
-				//printf("Envoyé: %s de taille %d\n",message,sent);
 			}
-			
 			gettimeofday(&end, NULL);
-			delta = ((end.tv_sec - start.tv_sec) * 1000.0f + (end.tv_usec - start.tv_usec) / 1000.0f) / 1000.0f;
-			
-			if ((f_out = fopen(SORTIE,"wb"))==NULL){
-				printf("Erreur: Impossible de lire le fichier de sortie (client)\n");
+			delta = ((end.tv_sec - start.tv_sec) * 1000.0f
+					+ (end.tv_usec - start.tv_usec) / 1000.0f) / 1000.0f;
+
+			if ((f_out = fopen(SORTIE, "wb")) == NULL) {
+				printf(
+						"Erreur: Impossible de lire le fichier de sortie (client)\n");
 			}
 
-			if ((i =(fwrite(buf_file,1,file_i,f_out))) == 0){
+			if ((i = (fwrite(buf_file, 1, file_i, f_out))) == 0) {
 				printf("Fichier demandé inexistant\n\n");
-			}
-			else{
+			} else {
 				printf("Fichier complet reçu en %f sec\n", delta);
-				printf("Nombre d'octets écrits: %d \n\n",i);
+				printf("Nombre d'octets écrits: %d \n\n", i);
 			}
-			fclose(f_out);	
-			
-			
+			fclose(f_out);
+
 			i = 0;
 			id_frag = 0;
-			delete(buf_file,sizeof(buf_file));
-			delete(buf,sizeof(buf));
+			delete(buf_file, sizeof(buf_file));
+			delete(buf, sizeof(buf));
 			file_i = 0;
 
-        }
+		}
 
+	}
 
-    }
-
-    close(s);
-    return 0;
+	close(s);
+	return 0;
 }
--- Jady/server.c
+++ Jady/server.c
@@ -1,148 +1,154 @@
 /*
-   Simple udp server
-http://www.binarytides.com/programming-udp-sockets-c-linux/
-*/
+ Simple udp server
+ http://www.binarytides.com/programming-udp-sockets-c-linux/
+ */
 
 #include "useful.h"
 
+int main(void) {
+	struct sockaddr_in si_me; //server struct addr
+	struct sockaddr_in si_other; //client struct addr
+	int s; //socket
+	int slen = sizeof(si_other);
+	char *buf = (char*) malloc(sizeof(char) * BUFLEN); //recu
+	char *message = (char*) malloc(sizeof(char) * FRAGLEN); //envoyé
+	int i; //filling the temporary little buffer
+	int c; //useful to get the char in the file
+	int PORT2 = PORT;
+	int pid_fils = -1;
+	int compare = 1; // il y a une difference
+	FILE * f_in;
+	int id_frag = 0, lastACK = 0;
+	int read = FRAGLEN, sent = 0, recv = 0;
+	char * ENTREE = (char*) malloc(sizeof(char) * FRAGLEN); //what server is sending
 
-int main(void)
-{
-	struct sockaddr_in si_me;//server struct addr
-    struct sockaddr_in si_other;//client struct addr
-    int s; //socket 
-    int slen = sizeof(si_other);
-    char *buf = (char*) malloc(sizeof(char) * BUFLEN); //recu
-    char *message = (char*) malloc(sizeof(char)* FRAGLEN); //envoyé
-    int i; //filling the temporary little buffer
-    int c; //useful to get the char in the file
-    int PORT2 = PORT;
-    int pid_fils = -1;
-    int compare = 1 ; // il y a une difference
-    FILE * f_in;
-    int id_frag = 0, lastACK = 0;
-    int read = FRAGLEN,sent=0,recv=0;
-    char * ENTREE = (char*) malloc(sizeof(char)* FRAGLEN); //what server is sending
-    
 	struct timeval end, start, beginread;
-	float RTT=0, counting=0, previous, display=0, displayer = 0;
-	char *id_frag_temp = (char*) malloc(sizeof(char)* 7);
-	char *res = (char*) malloc(sizeof(char)* FRAGLEN+7); //what's finally sent (id_frag + data)
-//
-    if (create_server(&s, &si_me, PORT) != 0){
-        die("Error on creating first server\n");
-    }
-    printf("I am %d \n",getpid());
-    //we clean up the buffer  
-    delete(buf,sizeof(buf));
+	float RTT = 0, counting = 0, previous, display = 0, displayer = 0;
+	char *id_frag_temp = (char*) malloc(sizeof(char) * 7);
+	char *res = (char*) malloc(sizeof(char) * FRAGLEN + 7); //what's finally sent (id_frag + data)
 
-    //keep listening for data
-    while(1){
-        delete(buf,strlen(buf));
-        receive_message(s, buf,(struct sockaddr *) &si_other, &slen);
-        //printf("Received by %d : %s\n",getpid(),buf);
-        if(compare !=0){//acceptation de connexion
-            compare = strcmp("SYN",buf);
-            if(compare==0){
-                PORT2 ++;
+	int rwnd = 1; //receiver window
+	int cwnd = 10; //congestion window
+	int flightSize = 0; //data that has been sent, but not yet acknowledged
 
-                sprintf(message, "SYN-ACK%d",PORT2);
+	int j = 0;
+	//
+	if (create_server(&s, &si_me, PORT) != 0) {
+		die("Error on creating first server\n");
+	}
+	printf("I am %d \n", getpid());
+	//we clean up the buffer
+	delete(buf, sizeof(buf));
 
-                send_message(s,message,15,  (struct sockaddr *)&si_other, slen);                
-                pid_fils = fork();
+	//keep listening for data
+	while (1) {
+		delete(buf, strlen(buf));
+		receive_message(s, buf, (struct sockaddr *) &si_other, &slen);
+		printf("Received by %d : %s\n", getpid(), buf);
+		if (compare != 0) { //acceptation de connexion
+			compare = strcmp("SYN", buf);
+			if (compare == 0) {
+				PORT2++;
 
-                if(pid_fils==0){
-                    printf("FILS %d de port %d\n",getpid(),PORT2);
+				sprintf(message, "SYN-ACK%d", PORT2);
 
-                    //On recrée un nouveau socket	
-                    if (create_server(&s, &si_me, PORT2) != 0){
-                        die("Error on creating a son server\n");
-                    }
-                }else{
-                    printf("PERE %d de port %d\n",getpid(),PORT);
-                    compare = 1;
-                    continue;					
-                }                
-            }
-            else{
-                compare = 1;				
-            }
-        }
-        else{ //une fois la connexion acceptée
-            ENTREE = buf;
-            if (access(ENTREE,0) == 0){
-				//sends a "OK" to say that the 
+				send_message(s, message, 15, (struct sockaddr *) &si_other,
+						slen);
+				pid_fils = fork();
+
+				if (pid_fils == 0) {
+					printf("FILS %d de port %d\n", getpid(), PORT2);
+
+					//On recrée un nouveau socket
+					if (create_server(&s, &si_me, PORT2) != 0) {
+						die("Error on creating a son server\n");
+					}
+				} else {
+					printf("PERE %d de port %d\n", getpid(), PORT);
+					compare = 1;
+					continue;
+				}
+			} else {
+				compare = 1;
+			}
+		} else { //une fois la connexion acceptée
+			ENTREE = buf;
+			if (access(ENTREE, 0) == 0) {
+				//sends a "OK" to say that the
 				//send_message(s,"OK",2,(struct sockaddr *)&si_other, slen);
-				
-				printf("\nEnvoi du fichier %s par %d...\n",ENTREE,getpid());
-			
-				if ((f_in = fopen(ENTREE,"rb")) == NULL){//b because nbinary
+
+				printf("\nEnvoi du fichier %s par %d...\n", ENTREE, getpid());
+
+				if ((f_in = fopen(ENTREE, "rb")) == NULL) {	//b because nbinary
 					die("Probleme ouverture fichier\n");
 				}
-				
+
 				gettimeofday(&beginread, NULL);
-				
-				while (1){
-					if (lastACK == id_frag){
-						read = fread(message,1,FRAGLEN,f_in);
+
+				while (1) {
+					if (flightSize < cwnd) {
+
+						read = fread(message, 1, FRAGLEN, f_in);
 						id_frag++;
-					}
-					//printf("\nRead: %d\n",read);
-					
-					if (read > 1){
-						gettimeofday(&start, NULL);
-						
-						sprintf(res, "%0.6d%s\n",id_frag,message);//the 6 first digits are for the id_frag
-						sent = send_message(s,res,read,(struct sockaddr *)&si_other, slen);
-						//printf("Sent: %d by %d\n",sent, getpid());
-						
-						recv = rcv_msg_timeout(s, buf,(struct sockaddr *) &si_other, &slen);
-						//printf("Recv: %s de taille %d\n",buf, recv);
-						lastACK = atoi(index(buf,'K')+1);
-						//printf("lastACK: %d\n",lastACK);
-						
+						//printf("same id_frag %d \n", read, id_frag);
+						//printf("\nRead: %d\n",read);
+						if (read > 1) {
+							gettimeofday(&start, NULL);
+
+							sprintf(res, "%0.6d%s\n", id_frag, message);//the 6 first digits are for the id_frag
+							sent = send_message(s, res, read,
+									(struct sockaddr *) &si_other, slen);
+							//printf("Sent: %d by %d\n", sent, getpid());
+							flightSize++;
+						} else {
+
+							break;
+						}
+					} else {
+						recv = rcv_msg_timeout(s, buf,
+								(struct sockaddr *) &si_other, &slen);
+						printf("Recv: %s de taille %d\n", buf, recv);
+						flightSize = 0;
+
+						lastACK = atoi(index(buf, 'K') + 1);
+						printf("lastACK: %d\n", lastACK);
+
 						gettimeofday(&end, NULL);
-						RTT = ((end.tv_sec - start.tv_sec) * 1000.0f + (end.tv_usec - start.tv_usec) / 1000.0f) / 1000.0f;
-						counting = ((end.tv_sec - beginread.tv_sec) * 1000.0f + (end.tv_usec - beginread.tv_usec) / 1000.0f) / 1000.0f;
-						
-						
+						RTT = ((end.tv_sec - start.tv_sec) * 1000.0f
+								+ (end.tv_usec - start.tv_usec) / 1000.0f)
+								/ 1000.0f;
+						counting = ((end.tv_sec - beginread.tv_sec) * 1000.0f
+								+ (end.tv_usec - beginread.tv_usec) / 1000.0f)
+								/ 1000.0f;
+
 						//display every second BUT increases RTT (x2 or x3) -> need another independant process
-						if(recv>0 && (previous!= (int)counting % read)){
-							display = (int)counting % read;
+						if (recv > 0 && (previous != (int) counting % read)) {
+							display = (int) counting % read;
 							printf("RTT: %f secondes\n", RTT);
 							previous = display;
 						}
-						
-						
 					}
-					else{
-						
-						break;
-					}
-					
-					
 				}
-				fseek(f_in, 0L, SEEK_END);
-				printf("Fichier de taille %d transmis avec succès\n\n",ftell(f_in));
+
+				printf("Fichier de taille %d transmis avec succès\n\n",
+						ftell(f_in));
 				fclose(f_in);
-				send_message(s,"FIN",3,  (struct sockaddr *)&si_other, slen);
-				delete(buf,strlen(buf));
-			}
-			else{
-				if (strcmp(" ",ENTREE) != 0 && strcmp("",ENTREE) != 0){
-					send_message(s,"FIN",3,  (struct sockaddr *)&si_other, slen);
+				send_message(s, "FIN", 3, (struct sockaddr *) &si_other, slen);
+				delete(buf, strlen(buf));
+			} else {
+				if (strcmp(" ", ENTREE) != 0 && strcmp("", ENTREE) != 0) {
+					send_message(s, "FIN", 3, (struct sockaddr *) &si_other,
+							slen);
 					printf("Le fichier '%s' n'existe pas.\n", ENTREE);
-					delete(buf,strlen(buf));
-					
-				}else{
-					//printf("Pourquoi envoyer du vide?\n");
+					delete(buf, strlen(buf));
+
+				} else {
+					printf("Pourquoi envoyer du vide?\n");
 				}
 			}
-        }
-    }
-    close(s);
+		}
+	}
+	close(s);
 
 }
-
-
 
